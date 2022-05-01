396397303e4dce03eaaba4fcc0861b904c929705
Habibun Noby <habibun.noby@gmail.com>
2022-05-01 11:21:38 +0600

    feat: add translation component

---
+++ cmd.sh
@@ -0,0 +1,6 @@
+ php bin/console translation:update --dump-messages fr
+ php bin/console translation:update --force fr
+ php bin/console translation:update --force en
+ php bin/console translation:update --help
+ php bin/console translation:update --clean fr --dump-messages
+ php bin/console translation:update --clean fr --dump-messages --force
--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,18 +3,31 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class HomepageController extends AbstractController
 {
     /**
-     * @Route("/", name="app_homepage")
+     * @Route("/{_locale}", name="app_homepage", requirements={"_locale": "en|de|fr"})
      */
-    public function index(): Response
+    public function index(Request $request, TranslatorInterface $translator): Response
     {
+//        $translated = $translator->trans('Symfony is great');
+//        $locale = $request->getLocale();
+
+        $count = rand(1, 100);
+        $name = sprintf('name_%d', $count);
+        $translated = $translator->trans('Hello '.$name);
+
+//        dd($translated);
+
         return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
+            'translated' => $translated,
+            'name' => $name,
+            'count' => $count,
         ]);
     }
 }
